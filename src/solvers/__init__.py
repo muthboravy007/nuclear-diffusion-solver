59dd9a762fc6a0ba6389e538a1e27f6ec3570689
MUTH Boravy <muthboravy007@gmail.com>
2026-01-13 00:12:25 +0700

    Initialize project structure for diffusion equation solver

--- src/solvers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    Nuclear Diffusion Solver Package
-    Day 1 - Jan 12th 2025 : Pack inititalization
-"""
-
-__version__ = "0.1.0"
-__author__  = "MUTH Boravy"
-
-print("Nuclear Diffusion Solver Package Initialized")
